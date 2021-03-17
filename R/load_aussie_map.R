92731c53704426f3c90bd8e7879e91d8768bce41
carlosyanez <carlos.yanez.s@gmail.com>
2021-03-17 15:42:47 +0100

    added regions

--- R/load_aussie_map.R
+++ R/load_aussie_map.R
@@ -7,8 +7,8 @@
 #' @import tibble
 #' @param filter_table table to filter (you can start with location_table)
 #' @param aggregation name of column to aggregate (POA_CODE16, LOCALITY,LGA)
-#' @export load_aussie_map
-load_aussie_map <- function(filter_table,aggregation=c("none")){
+#' @export load_map
+load_map <- function(filter_table,aggregation=c("none")){
 
   ###auxiliary function
   loadRData <- function(fileName){
@@ -17,22 +17,20 @@ load_aussie_map <- function(filter_table,aggregation=c("none")){
     get(ls()[ls() != "fileName"])
   }
 
-    States  <- filter_table %>% select(State) %>%
-               left_join(state_names,by="State") %>%
-               mutate(State_new=if_else(!is.na(State_short),State_short,State)) %>%
-               pull(State_new) %>% unique(.)
+     States  <- filter_table %>% select(State) %>%
+                left_join(state_names,by="State") %>%
+                mutate(State_new=if_else(!is.na(State_short),State_short,State)) %>%
+                pull(State_new) %>% unique(.)
 
       data <- map_df(States,function(x){
       datai<- loadRData(str_c("inst/extdata/",tolower(x),"_lga_loc_poa.rda"))
 
-      cols_filter <- filter_table %>% select(-State)
-      cols <- colnames(cols_filter)
+      data_cols <- colnames(as.data.frame(datai) %>% select(-State,-geometry))
+      cols_filter <- colnames(filter_table %>% select(any_of(data_cols)))
 
-      if(length(cols)>0){
 
-        cols_filter <- cols_filter %>% mutate(check=TRUE)
-        datai <- suppressMessages(datai %>% inner_join(cols_filter, by=cols) %>%
-                                            select(-check))
+      if(length(cols_filter)>0){
+        datai <- suppressMessages(suppressWarnings(datai %>% inner_join(filter_table %>% select(-State), by=cols_filter)))
       }
 
       datai
@@ -41,12 +39,22 @@ load_aussie_map <- function(filter_table,aggregation=c("none")){
 
     if(!(aggregation[1]=="none")){
 
-      data <- suppressWarnings(data %>%
+      data <- suppressMessages(suppressWarnings(data %>%
         group_by_at(aggregation) %>%
-        summarise(.groups = "drop"))
+        summarise(.groups = "drop")))
 
     }
 
 return(data)
+
+}
+
+#' Get list of all aggregation options
+#' @return no output
+#' @import dplyr
+#' @export list_agreggations
+list_agreggations <- function(){
+
+    colnames(locations.table)
 
 }
---
+++ R/regions.R
@@ -0,0 +1,25 @@
+#' List all pre-defined regions
+#' @return list of all regions
+#' @import dplyr
+#' @import tibble
+#' @export list_regions
+list_regions <- function(){
+          load("inst/extdata/regions.rda")
+          regions %>% arrange(Region) %>% pull(.) %>% unique(.)
+
+}
+
+#' Obtain tibble with areas from a particular region - to be used with load_map()
+#' @import dplyr
+#' @import tibble
+#' @param  region_name string with name of the region (use list_regions() to get all possible names)
+#' @return tibble to
+#' @export get_region_list
+get_region_list <- function(region_name){
+
+              load("inst/extdata/regions.rda")
+
+              regions %>% filter(Region %in% region_name) %>% select(LGA_PID,State,Region) %>% unique(.)
+
+
+}
