9a81b4dbf20477f911bd1bad7c1fc1fe840ebc4a
carlosyanez <carlos.yanez.s@gmail.com>
2021-04-11 21:16:38 +0200

    map resize

--- hex/hexSitcker.R
+++ hex/hexSitcker.R
@@ -7,20 +7,23 @@ librarian::shelf("tidyverse",
                  "ggfx",
                  "hexSticker",
                  "carlosyanez/customthemes",
-                 "carlosyanez/aussiemaps",
+                 "ozmaps",                   # aussiemaps is very heavy, using ozmaps for logo! (thanks ozmaps!)
                  "sf",
 )
 
 
-all_aussie_map <-locations.table %>% load_map(aggregation="State")
+#aussie_map <-locations.table %>% load_map(aggregation="State")
+aussie_map <- ozmap_data("states")
 
 p <- ggplot() +
-    geom_sf(data=all_aussie_map,aes(geometry=geometry),fill="#D22631",colour="#000000") +
+    geom_sf(data=aussie_map,aes(geometry=geometry,fill=NAME)) +
     custom_map_theme(legend_pos = "none") +
-   scale_fill_ochre()
+    scale_fill_manual(values=c(rep("#D22631",3),rep("#000000",3),rep("#FFC72C",3)))
 
-s <- sticker(p, package="aussiemaps", p_size=20, s_x=1, s_y=.75, s_width=1.3, s_height=1,
-             h_fill="#FFCD00", h_color="#00843D",
+height <- 1.6
+
+s <- sticker(p, package="aussiemaps", p_size=6, s_x=1, s_y=.75, s_width=1.3*height, s_height=height,
+             h_color="#FFCD00", h_fill="#00843D",
              filename=here("hex","hexSticker.png"))
 
-ggsave(s,here("hex","hexSticker.png"))
+
