186356c957d662562d0e20ced92be87b8db70b43
carlosyanez <carlos.yanez.s@gmail.com>
2021-09-20 12:03:31 +0200

    vignette changes

---
+++ inst/doc/aussiemaps.R
@@ -0,0 +1,117 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, include=FALSE-----------------------------------------------------
+#load packages
+
+if(!require(librarian)) install.packages("librarian")
+librarian::shelf("tidyverse","sf","carlosyanez/aussiemaps","carlosyanez/customthemes","
+                 osmdata","paletteer","openscilabs/ochRe","devtools","here")
+
+## ----arbitrarymap, eval=FALSE, include=TRUE-----------------------------------
+#  
+#  melb_inner1_filter<- tibble(LGA=c("Melbourne","Port Phillip","Stonnington"),State=rep("VIC",3))
+#  
+#  melb_inner1 <-  load_map(melb_inner1_filter,aggregation = "LGA")
+#  
+#  melb_inner1.centroid <- st_centroid(melb_inner1)
+#  
+#  #works wiht pipes too
+#  melb_inner2 <- tibble(LOCALITY=c("South Yarra"),State=c("VIC")) %>%
+#                       load_map()
+#  
+#  melb_inner3 <- tibble(POA_CODE16=c("3004"),State=c("VIC")) %>%
+#                       load_map()
+#  
+#  p1 <- ggplot() +
+#    geom_sf(data=melb_inner1,colour="white",size=1.2,fill="grey") +
+#    geom_sf(data=melb_inner2,aes(fill=LOCALITY),alpha=0.8,colour="white",size=0.8) +
+#    geom_sf(data=melb_inner3,aes(fill=POA_CODE16),alpha=0.8,colour="white",size=0.8) +
+#    geom_sf_text(data = melb_inner1.centroid, aes(label = LGA), colour = "blue") +
+#    scale_fill_ochre("lorikeet") +
+#    custom_map_theme() +
+#    labs(title="Arbitrary selection of South Yarra and Postal Area 3004",
+#         caption="Sources:data.gov.au, ABS")
+#  
+#  
+#  
+
+## ----eval=FALSE, include=FALSE------------------------------------------------
+#  ggsave(here("img","map1.png"),p1)
+
+## ----riversintheriverina, eval=FALSE, include=TRUE----------------------------
+#  
+#  #get regions
+#  riverina <- get_region("The Riverina") %>% load_map(aggregation=c("LGA","State","Region"))
+#  
+#  #draw contour for filtering
+#  riverina_contour <- riverina %>%
+#                       group_by(Region) %>% summarise(.groups = "drop") %>%
+#                       clean_polygons()
+#  
+#  #get rivers from OSM
+#  
+#  riverina_bbox <- st_bbox(riverina)
+#  
+#  rivers <- opq(riverina_bbox) %>%
+#          add_osm_feature(key = "waterway",
+#                    value = c("river","canal","creeks")) %>%
+#          osmdata_sf()
+#  
+#  rivers <- rbind(rivers$osm_lines %>% select(osm_id,name),
+#                  rivers$osm_multilines%>% select(osm_id,name)) %>%
+#            st_transform(st_crs(riverina_contour)) %>%
+#            st_intersection(riverina_contour)
+#  
+#  #plot
+#  
+#  p2<- ggplot() +
+#    geom_sf(data=riverina, aes(fill=LGA), colour="white", size=0.5) +
+#    geom_sf(data=rivers,colour="blue",size=1.1)+
+#    custom_map_theme()+
+#    scale_fill_ochre()  +
+#    labs(title="Rivers and Canals in The Riverina",
+#         caption="Sources:data.gov.au (map), openstreetmap (rivers)")
+#  
+#  
+#  
+#  
+
+## ----eval=FALSE, include=FALSE------------------------------------------------
+#  ggsave(here("img","map2.png"),p2)
+
+## ----melbournehousing, eval=FALSE, include=TRUE-------------------------------
+#  
+#  # load dataset from github
+#  url <- "https://github.com/dipalira/Melbourne-Housing-Data-Kaggle/raw/master/Data/Melbourne_housing_FULL.csv"
+#  melb_housing <- read_csv(url)
+#  
+#  # get median price by suburb
+#  melb_price <- melb_housing %>% filter(Type=="h") %>%
+#                group_by(Suburb) %>%
+#                summarise(medianprice=median(Price,na.rm=TRUE),.groups="drop")
+#  
+#  #join get sf object and add median prices
+#  Melbourne <- get_region("Greater Metropolitan Melbourne") %>%
+#               load_map(aggregation=c("LOCALITY","Metro.Region")) %>%
+#               left_join(melb_price,by=c("LOCALITY"="Suburb")) %>%
+#               filter(Metro.Region %in% c("Inner Melbourne","Metropolitan Melbourne"))
+#  
+#  #plot
+#  p3<- Melbourne %>% ggplot() +
+#    geom_sf(aes(fill=medianprice), colour="white", size=0.5) +
+#     custom_map_theme()+
+#      theme(legend.position = "right",
+#            legend.direction = "vertical") +
+#      scale_fill_gradient2(name="Median Price",
+#                           labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
+#      labs(title="Median House Price in Melbourne's Housing Kaggle Dataset",
+#           caption="Sources:data.gov.au (map), Github (data)")
+#  
+
+## ----eval=FALSE, include=FALSE------------------------------------------------
+#  ggsave(here("img","map3.png"),p3)
+
