8c011ffc58e30839d0ae2003dbfdfc855a8741ff
carlosyanez <carlos.yanez.s@gmail.com>
2023-04-19 17:42:02 +1000

    Update find_missing.R

--- data-raw/2016/find_missing.R
+++ data-raw/2016/find_missing.R
@@ -8,18 +8,31 @@ for(i in 1:nrow(b)){
   sa_code <- sa1 |> st_drop_geometry() |> pull(SA1_MAINCODE_2016 )
 
   existing <- data_base |> filter(SA1_MAINCODE_2016 ==sa_code)
+  if(nrow(existing)>0){
+
   existing <- tibble(a="sa_code",geom=st_union(existing)) |>
     st_as_sf() |>
     smoothr::fill_holes(units::set_units(1,"km^2"))
+  existing <- existing |> st_make_valid()
   sa1 <- sa1 |> st_make_valid()
 
   tryCatch({
-    diff <-abs(as.numeric((st_area(existing)- st_area(sa1))/st_area(sa1)))
+    exist_area  <- as.numeric(st_area(existing))
+    sa1_area    <- as.numeric(st_area(sa1))
 
-    if(diff<10^5){
+    diff        <- abs((exist_area-sa1_area)/sa1_area)
+
+    if(diff<10^-5){
       message(glue::glue("{i} out of {nrow(b)}: Equal Area"))
+      base_i<-tibble()
     }else{
-    base_i <- st_difference(existing,sa1)
+
+    if(exist_area>sa1_area){
+      base_i <- st_difference(sa1,existing)
+    }else{
+      base_i <- st_difference(existing,sa1)
+    }
+
     base_i <- st_cast(base_i, "POLYGON")
 
     base_i$area <- st_area(base_i)
@@ -30,16 +43,16 @@ for(i in 1:nrow(b)){
     redo_flag <- FALSE
   },
   error=function(e){message(glue:::glue("{i} out of {nrow(b)}: didnt work"))})
-
   if(redo_flag){
     base_i <- sa1
     data_base <- data_base |> filter(SA1_MAINCODE_2016 !=sa_code)
 
   }
-
-  if(nrow(base_i)==0){
-    message("no missing bits")
   }else{
+    message(glue::glue(("{i} out of {nrow(b)}: missing SA1")))
+    base_i <- sa1
+  }
+  if(nrow(base_i)>0){
   if(is.null(base)){
     base <- base_i
   }else{
@@ -54,4 +67,4 @@ for(i in 1:nrow(b)){
 base <- base[st_is(base |> st_make_valid(),c("POLYGON","MULTIPOLYGON")),]
 base <- base |> select(-a,-area)
 keep_vars <- unique(c(ls(),"keep_vars"))
-base <- base |> rename("geom"="shape")
+base <- base |> select(-geom)|>rename("geom"="shape")
